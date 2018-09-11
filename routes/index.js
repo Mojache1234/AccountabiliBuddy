e2420f071f51aa93d8139f165e0fff8f6f0f1c0f
Emma Brown <EmmaEmCreates@gmail.com>
2018-09-11 13:30:49 -0700

    Add partials & login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res){
+  res.render('signup')
+})
+
 module.exports = router;
