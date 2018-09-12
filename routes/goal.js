7c90214e0c495b297cf17bb25bb4de583fc04e27
Emma Brown <EmmaEmCreates@gmail.com>
2018-09-12 10:07:53 -0700

    styling goal page

--- routes/goal.js
+++ routes/goal.js
@@ -5,10 +5,25 @@ var appid = "921b848f07654d3288ba264ea63aaa58"
 
 
 router.get('/goal/:goalId', function(req, res){
+
   const goalId = req.params.goalId
   console.log(goalId)
-  res.render('goal', {goalId})
+
+  const dummyData = {
+    title: "Workout",
+    description: "Gonna go to the gym and do all the things and get ripped."
+
+  }
+  res.render('goal', {dummyData})
 })
+
+// 'channel_key': 'test_key2',
+//             'buddy': 'mojache1234@gmail.com',
+//             'goal_title': 'Work Out',
+//             'goal_desc': 'Pump 5 sick ass pushups',
+//             'commitment': 'call me maybe <3',
+//             'consequence': 5.0,
+//             'datetime': str(datetime.datetime.now() + datetime.timedelta(days=6))
 
 module.exports = router;
 
